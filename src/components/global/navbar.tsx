290c919367630944f9a47cc6a5f981fc10b55840
Vansh Vasishtha <vansh19092002@gmail.com>
2024-05-01 21:33:56 +0530

    Navbar

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,4 +1,12 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { MenuIcon } from 'lucide-react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { MenuIcon } from "lucide-react";
+
+type Props = {};
+
+const navbar = (props: Props) => {
+    return(
+        <div>Navbar</div>
+    )
+};
