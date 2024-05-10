ffcd942527e96b693276c8f636bb5731bacd68d7
Vansh Vasishtha <vansh19092002@gmail.com>
2024-05-10 22:26:15 +0530

    Navbar CHanges

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -5,8 +5,10 @@ import { MenuIcon } from "lucide-react";
 
 type Props = {};
 
-const navbar = (props: Props) => {
-    return(
-        <div></div>
-    )
+const Navbar = async (props: Props) => {
+  return (
+    <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between"></header>
+  );
 };
+
+export default Navbar;
