d8d8a2ea364b0985cfd08b2d860ea4ae631acf74
Vansh Vasishtha <vansh19092002@gmail.com>
2024-05-29 21:53:37 +0530

    Testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ModeToggle />
+      <p className="text-3xl font-bold">Testing</p>
     </main>
   );
 }
