331c94f82228d3a75e41a6fcf73db7816d61847c
Vansh Vasishtha <vansh19092002@gmail.com>
2024-05-30 20:54:56 +0530

    Navbar and assests added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { ModeToggle } from "@/components/global/mode-toggle";
+import Navbar from "@/components/global/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ModeToggle />
-      <p className="text-3xl font-bold">Testing</p>
+     <Navbar />
     </main>
   );
 }
