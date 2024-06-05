8a606e0c86b6e6634228cfa4e4fd14f6ec50d6cb
Vansh Vasishtha <vansh19092002@gmail.com>
2024-06-05 10:53:27 +0530

    Design Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,8 @@ export default function Home() {
       <ModeToggle />
       <Navbar />
       <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
-        <div>
-          
-        </div>
+      <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]"></div>
       </section>
     </main>
   );
